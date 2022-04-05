c6656ce9abbf95900adb1e88c5ae04e1555da597
Hung Nguyen <hungnguyen10897@gmail.com>
2022-04-05 20:13:40 +0300

    Read Events from Kafka into Streaming app

---
+++ code/mysimpbdp/init.sh
@@ -0,0 +1,3 @@
+docker-compose -f kafka-docker-compose.yml up 
+
+bin/kafka-topics.sh --list --bootstrap-server localhost:29092
---
+++ code/tenant/init.sh
@@ -0,0 +1,2 @@
+# Set Up Cassandra table that stores tenant data
+./cqlsh-astra/bin/cqlsh  -u k8ssandra-superuser -p sZJMiTFETe7yc1F3hBT0 -f tenant-table.cql
