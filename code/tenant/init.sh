a4270c23afe006bc8ce232154b2b6ed6136978dc
Hung Nguyen <hungnguyen10897@gmail.com>
2022-04-07 19:00:53 +0300

    Finish tenantstreamapp

--- code/tenant/init.sh
+++ code/tenant/init.sh
@@ -1,2 +1,11 @@
 # Set Up Cassandra table that stores tenant data
-./cqlsh-astra/bin/cqlsh  -u k8ssandra-superuser -p sZJMiTFETe7yc1F3hBT0 -f tenant-table.cql
+./cqlsh-astra/bin/cqlsh  -u k8ssandra-superuser -p czFO48OolLkkmhoouAGe -f tenant-table.cql
+
+# Run tenantstreamapp
+spark-submit  \
+--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.2 \
+--packages com.datastax.spark:spark-cassandra-connector_2.12:3.0.1 \
+--conf spark.sql.extensions=com.datastax.spark.connector.CassandraSparkExtensions \
+tenantstreamapp.py
+
+# There might be issues with maven or ivy caches, remove ~/.ivy2
--- code/tenant/tenantstreamapp.py
+++ code/tenant/tenantstreamapp.py
@@ -1,5 +1,3 @@
-# spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.2 tenantstreamapp.py 
-
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 from pyspark.sql.functions import *
@@ -9,6 +7,12 @@ TOPIC = "tenant"
 spark = SparkSession \
     .builder \
     .appName("streamingApp") \
+    .config("spark.cassandra.connection.host", "localhost") \
+    .config("spark.cassandra.connection.port", "9042") \
+    .config("spark.cassandra.auth.username", "k8ssandra-superuser") \
+    .config("spark.cassandra.auth.password", "czFO48OolLkkmhoouAGe") \
+    .config("spark.sql.extensions", "com.datastax.spark.connector.CassandraSparkExtensions") \
+    .config("spark.sql.catalog.lh", "com.datastax.spark.connector.datasource.CassandraCatalog") \
     .getOrCreate()
 
 spark.sparkContext.setLogLevel("ERROR")
@@ -32,45 +36,37 @@ schema = StructType([ \
   ])
 
 # Local Dev
-# df = spark \
-#     .readStream \
-#     .schema(schema) \
-#     .format("csv") \
-#     .option("delimiter", "\t") \
-#     .load("./staging")
+df = spark \
+    .readStream \
+    .schema(schema) \
+    .format("csv") \
+    .option("delimiter", "\t") \
+    .load("./staging")
 
-rows = spark \
-  .readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "localhost:29092") \
-  .option("subscribe", TOPIC) \
-  .load()
-
-# Turn Kafka Message Values to normal Dataframe
-df = rows \
-  .selectExpr("CAST(value AS STRING)") \
-  .withColumn("jsonData",from_json(col("value"),schema)) \
-    .select("jsonData.*")
-
-# df.writeStream \
-#   .format("console") \
-#   .outputMode("update") \
-#   .start() \
-#   .awaitTermination() 
+df = df.withColumn("review_date", to_timestamp("review_date"))
 
 groupDF = df.select("product_id", "star_rating", "helpful_votes", "total_votes", "review_date") \
-        .groupBy("product_id", "review_date") \
+        .withWatermark("review_date", "1 day") \
+        .groupBy(
+          "product_id", 
+          window("review_date", "1 day")
+        ) \
         .agg( 
           avg("star_rating").alias("avg_star_rating"),
           sum("helpful_votes").alias("sum_helpful_votes"),
           sum("total_votes").alias("sum_total_votes"),
           count("star_rating").alias("number_of_reviews")
-        ) \
-        # .where("number_of_reviews > 1")
+        )
 
-groupDF.writeStream \
-  .format("console") \
-  .outputMode("update") \
+resultDF = groupDF.withColumn("review_date", to_date(groupDF.window.start)).drop("window")
+
+resultDF.printSchema()
+
+resultDF.writeStream \
+  .format("org.apache.spark.sql.cassandra") \
+  .option("keyspace", "mysimbdp") \
+  .option("table", "aggregated_reviews") \
+  .option("checkpointLocation", "./checkpoint") \
+  .outputMode("append") \
   .start() \
-  .awaitTermination() 
-
+  .awaitTermination()
